a1dc98ee57a3d57ec2474a211b05290907e604e8
Aleksandar Micic <aleksandar.micic@privax.com>
2015-12-17 13:33:58 +0100

    Fix references

--- EventListener/RateLimitAnnotationListener.php
+++ EventListener/RateLimitAnnotationListener.php
@@ -11,7 +11,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class RateLimitAnnotationListener extends BaseListener
 {
